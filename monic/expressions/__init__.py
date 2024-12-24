9a8f9eb2a4f18cb7bf2811831e86cbb2d1dd7c8e
Jaepil Jeong <jaepil@aeca.ai>
2024-12-24 22:07:54 +0900

    Export parser and interpreter

--- monic/expressions/__init__.py
+++ monic/expressions/__init__.py
@@ -3,3 +3,12 @@
 #
 # Copyright (c) 2024 Cognica, Inc.
 #
+
+from monic.expressions.interpreter import ExpressionInterpreter
+from monic.expressions.parser import ExpressionParser
+
+
+__all__ = [
+    "ExpressionInterpreter",
+    "ExpressionParser",
+]
