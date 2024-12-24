3be91a2ea9d4c38c99428ffc333a87a363c9aa86
Jaepil Jeong <jaepil@cognica.io>
2024-12-24 16:16:44 +0900

    Add interpreter.py

---
+++ monic/expressions/interpreter.py
@@ -0,0 +1,15 @@
+#
+# Monic Framework
+#
+# Copyright (c) 2024 Cognica, Inc.
+#
+
+import ast
+
+
+class ExpressionInterpreter:
+    def __init__(self):
+        pass
+
+    def execute(self, module: ast.Module) -> None:
+        pass
