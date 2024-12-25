6b9e39e2937eea584cec66b9d6b073a9fdaeabc1
Jaepil Jeong <jaepil@cognica.io>
2024-12-25 15:44:57 +0900

    Make timeout option optional

--- monic/expressions/context.py
+++ monic/expressions/context.py
@@ -4,10 +4,11 @@
 # Copyright (c) 2024 Cognica, Inc.
 #
 
+import typing as t
 from dataclasses import dataclass
 
 
 @dataclass
 class ExpressionContext:
     # The timeout for evaluating the expression in seconds.
-    timeout: float = 10.0
+    timeout: t.Optional[float] = 10.0
