e894ad4962857307670e287a90fd51a329ac0e22
Jaepil Jeong <jaepil@aeca.ai>
2024-12-31 10:11:57 +0900

    Add type annotations to json module

--- monic/runtime/json.py
+++ monic/runtime/json.py
@@ -5,15 +5,16 @@
 #
 
 import json
+import typing as t
 
 from monic.expressions.registry import monic_bind_default
 
 
 @monic_bind_default("json.dumps")
-def json_dumps(obj, *args, **kwargs):
+def json_dumps(obj, *args, **kwargs) -> str:
     return json.dumps(obj, *args, **kwargs)
 
 
 @monic_bind_default("json.loads")
-def json_loads(s, *args, **kwargs):
+def json_loads(s, *args, **kwargs) -> t.Any:
     return json.loads(s, *args, **kwargs)
