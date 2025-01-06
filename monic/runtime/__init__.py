dde0888cec1ac38c4b8b3ce7313ee3bd04699922
Jaepil Jeong <jaepil@aeca.ai>
2025-01-06 12:43:37 +0900

    Add Arrow module

--- monic/runtime/__init__.py
+++ monic/runtime/__init__.py
@@ -4,6 +4,7 @@
 # Copyright (c) 2024-2025 Cognica, Inc.
 #
 
+from monic.runtime import arrow
 from monic.runtime import datetime
 from monic.runtime import json
 from monic.runtime import numpy
@@ -13,6 +14,7 @@ from monic.runtime import time
 
 
 __all__ = [
+    "arrow",
     "datetime",
     "json",
     "numpy",
