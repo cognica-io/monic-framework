797746a492e359455c6a2414c0c30b216c0beffc
Jaepil Jeong <jaepil@cognica.io>
2025-01-25 10:26:03 +0900

    Update version

--- monic/version.py
+++ monic/version.py
@@ -4,7 +4,7 @@
 # Copyright (c) 2024-2025 Cognica, Inc.
 #
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __author__ = "Cognica, Inc."
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright (c) 2024-2025 Cognica, Inc."
